b423a955a6fe14d2fef3dea2649f08768345f019
Alix Pelletier <alixpelletierpro@gmail.com>
2020-07-26 10:23:22 +0200

    modify css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create a "close" button and append it to each list item
+// Création d'un bouton "close" pour chaque élément de la liste
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -9,7 +9,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+// Le bouton "close cache l'élément de la liste au click"
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,7 +19,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
+// Ajoute une icône "checked" au click de l'élément
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -27,13 +27,13 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
+// Crée un élément au click du bouton "Ajouter"
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === '') { // si l'input n'a pas été rempli :
     alert("écris avant de cocher 🙄 !");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -41,12 +41,12 @@ function newElement() {
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  var txt = document.createTextNode("\u00D7");// \u00D7 = signe x
+  span.className = "close"; // ajout de class au hover
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
+  for (i = 0; i < close.length; i++) { // suppression de l'élément tant qu'il y en a un
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
